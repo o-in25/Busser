173b075db52e12cca2e7a246f7258032b7dea52c
o-in25 <ehalligan12@gmail.com>
2024-04-03 17:59:19 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+
+<div class="container mx-auto px-4">
+    <slot/>
+</div>
+<script lang="ts">
+    import "../app.css";
+    import type { LayoutData } from './$types';
+    import { BottomNav, BottomNavItem, Skeleton, ImagePlaceholder } from 'flowbite-svelte';
+    import { HomeSolid, WalletSolid, AdjustmentsVerticalOutline, UserCircleSolid } from 'flowbite-svelte-icons';
+
+    export let data: LayoutData;
+</script>
+
+<style lang="postcss">
+
+</style>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,30 @@
+<div class="flex justify-center p-10">
+  <Card>
+    <form class="flex flex-col space-y-6" action="/">
+      <h3 class="text-xl font-medium text-gray-900 dark:text-white">Sign in</h3>
+      <Label class="space-y-2">
+        <span>Email</span>
+        <Input type="email" name="email" placeholder="name@company.com" required />
+      </Label>
+      <Label class="space-y-2">
+        <span>Password</span>
+        <Input type="password" name="password" placeholder="•••••" required />
+      </Label>
+      <div class="flex items-start">
+        <Checkbox>Remember me</Checkbox>
+        <a href="/" class="ms-auto text-sm text-primary-700 hover:underline dark:text-primary-500"> Lost password? </a>
+      </div>
+      <Button type="submit" class="w-full">Sign in</Button>
+      <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+        Not registered? <a href="/" class="text-primary-700 hover:underline dark:text-primary-500"> Create account </a>
+      </div>
+    </form>
+  </Card>
+</div>
+
+<script lang="ts">
+    import type { PageData } from './$types';
+    import { Card, Button, Label, Input, Checkbox } from 'flowbite-svelte';
+
+    export let data: PageData;
+</script>
