7ee469280d7f1fdb541929a572b3a4a6aa54cf34
o-in25 <ehalligan12@gmail.com>
2024-07-10 23:31:18 -0400

    working table migration

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,5 @@
 import type { LayoutServerLoad } from './$types';
 export const load: LayoutServerLoad = async ({ locals }) => {
-  console.log(locals);
   let { user } = locals;
   user = JSON.parse(JSON.stringify(user));
   return { user };
--- src/routes/api/user/+server.ts
+++ src/routes/api/user/+server.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from './$types';
 import { DbProvider } from '$lib/server/db';
-const db = new DbProvider();
+const db = new DbProvider('user_t');
 
 export const GET: RequestHandler = async () => {
     return new Response();
