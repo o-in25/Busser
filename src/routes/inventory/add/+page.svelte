f04ca729c1112e88e994d8097d105dc909e90c86
o-in25 <ehalligan12@gmail.com>
2024-07-16 13:40:24 -0400

    working breadcrumbs

--- src/routes/inventory/add/+page.svelte
+++ src/routes/inventory/add/+page.svelte
@@ -3,27 +3,15 @@
     import type { PageData } from './$types';
     import { AngleRightOutline } from 'flowbite-svelte-icons';
     import InventoryForm from '$lib/components/InventoryForm.svelte';
+    import Breadcrumb from '$lib/components/Breadcrumb.svelte';
+    import BreadcrumbItem from '$lib/components/BreadcrumbItem.svelte';
     
     export let data: PageData;
 </script>
 
-<nav class="flex" aria-label="Breadcrumb">
-  <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-    <li class="inline-flex items-center">
-      <a href="/inventory" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-        Inventory
-      </a>
-    </li>
-    <li>
-      <div class="flex items-center">
-        <div class="text-gray-500 dark:text-gray-400">
-          <AngleRightOutline/>
-        </div>
-        <span class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400">Add To Inventory</span>
-      </div>
-    </li>
-  </ol>
-</nav>
+<Breadcrumb name="Inventory" href="/inventory">
+  <BreadcrumbItem name="Add To Inventory"></BreadcrumbItem>
+</Breadcrumb>
 <div class="px-4 pb-4 mt-3 bg-gray-50 rounded-lg dark:bg-gray-800">
   <div class="text-sm text-gray-500 dark:text-gray-400">
       <div class="flex justify-end">
