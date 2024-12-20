3b8d80bbe55eeac6d7300eb1918397a10983056d
o-in25 <ehalligan12@gmail.com>
2024-12-20 16:07:44 -0500

    added signed url check

--- src/lib/components/FancyImage.svelte
+++ src/lib/components/FancyImage.svelte
@@ -1,7 +1,36 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import { POST } from "../../routes/(auth)/[slug]/+server";
+
   export let src: string;
   export let alt: string;
+  export let divStyle: string =
+    "rounded-lg border border-gray-200 dark:border-gray-700 divide-gray-200 dark:divide-gray-700 shadow-md flex w-full max-w-sm flex-col";
+  export let imgStyle: string = "rounded-lg";
+  export let isUnsigned: boolean = false;
+
+  export let unsignedUrl = src; 
+  let signedUrl = '';
+
+  const getSignedUrl = async (unsignedUrl) => {
+    let data = await fetch(`/storage/get-signed-url`, {
+      method: "POST",
+      body: JSON.stringify({ unsignedUrl }),
+    });
+    let { signed } = await data.json();
+    return signed;
+  };
+
+  onMount(() => {
+    if(isUnsigned && unsignedUrl) {
+      getSignedUrl(unsignedUrl).then((signed) => {
+        signedUrl = signed;
+      });
+    }
+  });
+
 </script>
-<div class="rounded-lg border border-gray-200 dark:border-gray-700 divide-gray-200 dark:divide-gray-700 shadow-md flex w-full max-w-sm flex-col">
-  <img class="rounded-lg" {src} {alt}>
+
+<div class={divStyle}>
+  <img class={imgStyle} src={signedUrl || unsignedUrl} {alt} />
 </div>
