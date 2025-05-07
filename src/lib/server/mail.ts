861db78653d9259fb8c71057bd88ab24e2abc7f0
Eoin Halligan <eoin@losant.com>
2025-05-07 10:48:49 -0700

    working registration email test

--- src/lib/server/mail.ts
+++ src/lib/server/mail.ts
@@ -3,6 +3,7 @@ const { MAILGUN_KEY } = process.env;
 import FormData from "form-data"; // form-data v4.0.1
 import Mailgun from "mailgun.js"; // mailgun.js v11.1.0
 import { Logger } from "./logger";
+import { user } from "../../stores";
 
 const mailgun = new Mailgun(FormData);
 const mailClient = mailgun.client({
@@ -10,28 +11,18 @@ const mailClient = mailgun.client({
   key: MAILGUN_KEY || ''
 });
 
-
-export async function sendSimpleMessage(to: string[], subject: string) {
+export async function sendRegisterUserEmail(to: string[], username: string, token: string) {
   try {
-    const data = await mailClient.messages.create("busserapp.com", {
+    await mailClient.messages.create("busserapp.com", {
       from: "Busser <noreply@busserapp.com>",
       to,
-      subject,
-      template: "User Registration",
+      subject: "Welcome to Busser!",
+      template: "user registration",
       "h:X-Mailgun-Variables": JSON.stringify({
-        username: "test",
-        expiry: Logger.now()
+        username, token
       }),
     });
-  } catch (error) {
-    console.log(error); //logs any error
-  }
-}
-
-export async function sendRegisterUserEmail(to: string[], token: string) {
-  try {
-
   } catch(error: any) {
-
+    Logger.error(error.message)
   }
 }
--- src/routes/(auth)/check-email/+page.server.ts
+++ src/routes/(auth)/check-email/+page.server.ts
@@ -1,7 +1,12 @@
+import { signToken } from '$lib/server/auth';
+import { sendRegisterUserEmail } from '$lib/server/mail';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
-
+  const token = await signToken({
+    name: 'test'
+  })
+  await sendRegisterUserEmail(["eoinhalligan3@gmail.com"], "eoin", token)
 
 
 }) satisfies PageServerLoad;
