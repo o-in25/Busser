80cd17a6cc275e27bb0d3019ff70e064bcb82aa9
o-in25 <ehalligan12@gmail.com>
2026-02-26 20:44:47 -0500

    tailwind 4 migrate

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [tailwindcss(), sveltekit()],
 	ssr: {
 		noExternal: ['svelte-sonner'],
 		external: ['form-data', 'mailgun.js'],
