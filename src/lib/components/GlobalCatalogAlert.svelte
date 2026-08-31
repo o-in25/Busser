8b31c328208eb2b4811d78d7366249600fb7651d
Eoin Halligan <ehalligan12@gmail.com>
2026-08-31 16:05:58 -0400

    the smallest change ever

--- src/lib/components/GlobalCatalogAlert.svelte
+++ src/lib/components/GlobalCatalogAlert.svelte
@@ -10,7 +10,7 @@
 	{#snippet children()}
 		<p class="sm:hidden">Viewing global catalog</p>
 		<p class="hidden sm:block">
-			You're viewing <strong>Busser's global catalog</strong>. Use the workspace switcher to switch
+			You're viewing <strong>Busser's global catalog</strong>. Use the Workspace switcher to switch
 			to your own and manage your inventory.
 		</p>
 	{/snippet}
